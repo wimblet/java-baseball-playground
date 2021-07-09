f9508fd84df7b8f2159359dfb486e7edc87c795d
김민석 <wimblet@naver.com>
2021-07-09 17:46:45 +0900

    학습테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,52 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+    @DisplayName("샘플코드")
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @DisplayName("요구사항1-1")
+    @Test
+    void split_1() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @DisplayName("요구사항1-2")
+    @Test
+    void split_2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @DisplayName("요구사항2")
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @DisplayName("요구사항3-1")
+    @Test
+    void charAt_1() {
+        char actual = "abc".charAt(1);
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @DisplayName("요구사항3-2")
+    @Test
+    void charAt_2() {
+        assertThatThrownBy(()->{
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range:");
     }
 }
